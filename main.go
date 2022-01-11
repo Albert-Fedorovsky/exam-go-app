63c9f6ae28ef79314236fc35392659f6a6779ad6
Albert_Fedorovsky <alb271@yandex.ru>
2022-01-11 05:30:02 +0300

    Updated webpage

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ func (m msg) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
    fmt.Fprint(resp, m)
 }
 func main() {
-    msgHandler := msg("Hello world 2")
+    msgHandler := msg("<h1>Andersen DevOps cource</h1><h2>exam</h2><p>Hello world 2</p>")
     fmt.Println("Server is listening...")
     http.ListenAndServe("0.0.0.0:8181", msgHandler)
 }
